4a8dc38434298bf5d3dc90e42006c879dce007f2
Bora Korpe <borakorpee@gmail.com>
2023-01-08 18:53:06 +0300

    home screen ui

--- lib/components/appHeader.dart
+++ lib/components/appHeader.dart
@@ -3,48 +3,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Header extends StatelessWidget {
+class Header extends StatefulWidget {
   const Header({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xff3c8dbc),
-      width: 414.w,
-      height: 50.h,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Row(
-            children: const [
-              Icon(
-                Icons.currency_lira_rounded,
+    return Stack(
+      children: [
+        Container(
+          color: const Color(0xff3c8dbc),
+          width: 414.w,
+          height: 50.h,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: const [
+                  Icon(
+                    Icons.currency_lira_rounded,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  Text(
+                    "10,5",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+              SizedBox(width: 25.w),
+              const Icon(
+                Icons.shopping_cart,
                 color: Colors.white,
-                size: 16,
+                size: 18,
               ),
-              Text(
-                "10,5",
-                style: TextStyle(color: Colors.white, fontSize: 15),
+              SizedBox(width: 25.w),
+              SizedBox(
+                width: 25.w,
+                height: 25.w,
+                child: Image.network("https://yks.bakircay.edu.tr/img/O.png"),
               ),
+              SizedBox(width: 25.w),
             ],
           ),
-          SizedBox(width: 25.w),
-          const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
-            size: 18,
-          ),
-          SizedBox(width: 25.w),
-          SizedBox(
-            width: 25.w,
-            height: 25.w,
-            child: Image.network("https://yks.bakircay.edu.tr/img/O.png"),
-          ),
-          SizedBox(width: 25.w),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../components/appHeader.dart';
@@ -38,10 +39,71 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(children: const [
-        Header(),
-        Text("home"),
-      ]),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            color: const Color(0xffecf0f5),
+            child: Column(
+              children: [
+                const Header(),
+                SizedBox(height: 20.h),
+                Container(
+                  color: const Color(0xffecf0f5),
+                  child: Container(
+                    width: 335.w,
+                    height: 160.h,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15).r,
+                      child: Text(
+                        "Bakirçay Üniversitesi Turnike ve Yemekhane Sistemi mobil uygulamasına hoş geldiniz. Yandaki menüden ilgili adımları takip ederek işlemlerinizi yapabilirsiniz.",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18.sp),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            child: Container(
+              width: double.infinity,
+              height: 71.h,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: const Color(0xffd2d6de),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, top: 10),
+                child: RichText(
+                  text: const TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: "Copyright © 2018 ",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700)),
+                      TextSpan(
+                          text: " Bakırçay Üniversitesi",
+                          style: TextStyle(
+                              color: Color(0xff3c8dbc),
+                              fontWeight: FontWeight.w700)),
+                      TextSpan(
+                          text: " Sağlık Kültür ve Spor Daire Başkanlığı",
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w300))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
